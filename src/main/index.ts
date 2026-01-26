836278c3e5e002b5625f1d5b839311f527d35542
gemrey13 <gemreyranola@gmail.com>
2026-01-26 10:22:41 +0800

    config build

--- src/main/index.ts
+++ src/main/index.ts
@@ -33,7 +33,7 @@ function createWindow(): void {
 }
 
 app.whenReady().then(() => {
-  electronApp.setAppUserModelId("com.electron");
+  electronApp.setAppUserModelId("com.giligans.girecon");
 
   app.on("browser-window-created", (_, window) => {
     optimizer.watchWindowShortcuts(window);
