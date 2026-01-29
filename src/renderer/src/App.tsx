e06c9fcd21676edd9aa808dcab7724c3a290e9d8
gemrey13 <gemreyranola@gmail.com>
2026-01-29 13:15:35 +0800

    remove

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,6 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Overview from '@renderer/pages/Overview';
 import MainLayout from '@renderer/layouts/MainLayout';
-import ReconPage from '@renderer/pages/ReconPage';
 import GrabDashboard from '@renderer/pages/GrabDashboard';
 import GrabSessionDetail from '@renderer/pages/GrabSessionDetail';
 
@@ -12,8 +11,6 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Overview />} />
           <Route path="recon/grab" element={<GrabDashboard />} />
-          <Route path="recon/panda" element={<ReconPage partner="PANDA" />} />
-
           <Route path="recon/grab/:sessionId" element={<GrabSessionDetail />} />
         </Route>
       </Routes>
