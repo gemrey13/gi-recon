736a0b45f70424d24781a4abf4ceebf96fedf84e
gemrey13 <gemreyranola@gmail.com>
2026-03-17 16:10:17 +0800

    format

--- src/main/ipc/reconcileIpc.ts
+++ src/main/ipc/reconcileIpc.ts
@@ -1,12 +1,12 @@
-import { ipcMain } from 'electron'
-import { getGrabBranchOptions, grabPosReconciliation } from '../grabReconcile/grabRecon'
+import { ipcMain } from "electron";
+import { getGrabBranchOptions, grabPosReconciliation } from "../grabReconcile/grabRecon";
 
 export function registerReconcileIpc() {
-  ipcMain.handle('recon:grab-pos', async (_, filters) => {
-    return grabPosReconciliation(filters)
-  })
+  ipcMain.handle("recon:grab-pos", async (_, filters) => {
+    return grabPosReconciliation(filters);
+  });
 
-  ipcMain.handle('get-grab-branches', async () => {
-    return getGrabBranchOptions()
-  })
+  ipcMain.handle("get-grab-branches", async () => {
+    return getGrabBranchOptions();
+  });
 }
