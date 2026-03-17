ba3bb10f4618314ab1d08cd4945707c8161d310b
gemrey13 <gemreyranola@gmail.com>
2026-03-17 16:17:55 +0800

    format

--- src/main/ipc/index.ts
+++ src/main/ipc/index.ts
@@ -1,11 +1,11 @@
-import { registerGrabIpc } from "./grabIpc"
-import { registerMenuIpc } from "./menuIpc"
-import { registerPosIpc } from "./posIpc"
-import { registerReconcileIpc } from "./reconcileIpc"
+import { registerGrabIpc } from "./grabIpc";
+import { registerMenuIpc } from "./menuIpc";
+import { registerPosIpc } from "./posIpc";
+import { registerReconcileIpc } from "./reconcileIpc";
 
 export function registerAllIpc() {
-  registerPosIpc()
-  registerGrabIpc()
-  registerReconcileIpc()
-  registerMenuIpc()
+  registerPosIpc();
+  registerGrabIpc();
+  registerReconcileIpc();
+  registerMenuIpc();
 }
